1f9f058cf64558b54a40b76bb42c47b212f2f72b
sjp <sjp7878@gmail.com>
2021-07-25 17:34:01 +0900

    jenkins test

--- src/main/java/com/example/demo/blogics/HelloRestController.java
+++ src/main/java/com/example/demo/blogics/HelloRestController.java
@@ -2,8 +2,10 @@ package com.example.demo.blogics;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping(value = "/hello")
+@RestController
 public class HelloRestController {
 	
 	@GetMapping(value = "")
